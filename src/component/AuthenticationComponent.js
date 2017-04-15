86d7e0dfaee6d294d836f000438c29e8adfc647a
Anna Alimova <new@alimov.net>
2017-04-15 12:53:31 +0300

    Component added

---
+++ src/component/AuthenticationComponent.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Authentification from './component/Authentification';
+
+export default class Authentification extends Component {
+    constructor () {
+        super()
+        this.state = {
+
+        }
+    }
+}
+
+render(){
+    return(
+        <div>Authentification</div>
+    )
+}
