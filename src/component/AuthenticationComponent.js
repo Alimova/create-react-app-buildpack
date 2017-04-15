86348e25fb936d44cc2c7bedb8ca8e7be4f58d19
Anna Alimova <new@alimov.net>
2017-04-15 13:11:20 +0300

    react-create-app on Heroku

--- src/component/AuthenticationComponent.js
+++ src/component/AuthenticationComponent.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import Authentification from './component/Authentification';
+//import '../App.css'
 
 export default class Authentification extends Component {
     constructor () {
         super()
         this.state = {
-
         }
     }
 }
 
 render(){
     return(
-        <div>Authentification</div>
+        <div className="App">Authentification</div>
     )
 }
+
+export default App;
